72ec09cdb4bf4821127f884152c9746bc15ddeb7
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2022-03-28 14:40:08 +0300

    limiting
    Limiting Player Position

--- 3D Cube Game/Assets/Scripts/Player.cs
+++ 3D Cube Game/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float dodgeSpeed;
+    public float maxX;
     float xInput;
     void Start()
     {
@@ -17,5 +18,8 @@ public class Player : MonoBehaviour
        xInput = Input.GetAxis("Horizontal");
 
         transform.Translate(xInput*dodgeSpeed*Time.deltaTime,0,0);
+
+       float limitedX = Mathf.Clamp(transform.position.x,-maxX,maxX);
+        transform.position = new Vector3(limitedX,transform.position.y,transform.position.z);
     }
 }
