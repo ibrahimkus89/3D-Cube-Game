791f20ce7f3bce1e3ebb6ed2eb883a39538805ea
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2022-03-28 17:07:22 +0300

    spawning enemies
     Spawning Enemies from Random Position

---
+++ 3D Cube Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform spawnPoint;
+    public float maxSpawnPointX;
+
+
+    public static GameManager instance;
+
+
+
+    private void Awake()
+    {
+        if (instance ==null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.8f);
+            Spawn();
+        }
+    }
+
+
+    public void Spawn()
+    {
+        float randomSpawnX = Random.Range(-maxSpawnPointX,maxSpawnPointX);
+
+        Vector3 enemySpawnPos = spawnPoint.position;
+        enemySpawnPos.x = randomSpawnX;
+
+        Instantiate(enemy, enemySpawnPos, Quaternion.identity);
+    }
+}
